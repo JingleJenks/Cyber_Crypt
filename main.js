b78823ae19ec9d1bc2d9e1b46d67486c3b44f8b5
JingleJenks <sylv2439@stud.kea.dk>
2022-02-01 17:11:40 +0100

    1st Hex working w JS

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-drop_close();
+drop_close_HOW();
+drop_close_top5();
 
-function drop_close() {
-    console.log("hello");
+function drop_close_HOW() {
     document.querySelector(".drop_svg").addEventListener("click", openHow);
 
 
@@ -17,6 +17,27 @@ function drop_close() {
         document.querySelector(".p_how").classList.add("hidden")
         document.querySelector(".drop_svg").addEventListener("click", openHow);
         document.querySelector(".drop_svg").removeEventListener("click", closeHow);
+
+
+
+    }
+}
+
+function drop_close_top5() {
+    console.log("hello");
+    document.querySelector(".top5_1").addEventListener("click", open_1);
+
+
+    function open_1() {
+        document.querySelector(".drop_top5_1").classList.remove("hidden")
+        document.querySelector(".top5_1").addEventListener("click", close_1);
+        document.querySelector(".top5_1").removeEventListener("click", open_1);
+    }
+
+    function close_1() {
+        document.querySelector(".drop_top5_1").classList.add("hidden")
+        document.querySelector(".top5_1").addEventListener("click", open_1);
+        document.querySelector(".top5_1").removeEventListener("click", close_1);
 
 
 
