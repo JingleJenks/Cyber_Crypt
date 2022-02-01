4da25e17093f3896fb073f2467b790c4282c68f7
JingleJenks <sylv2439@stud.kea.dk>
2022-02-01 17:15:43 +0100

    2nd hex working

--- main.js
+++ main.js
@@ -26,20 +26,34 @@ function drop_close_HOW() {
 function drop_close_top5() {
     console.log("hello");
     document.querySelector(".top5_1").addEventListener("click", open_1);
+    document.querySelector(".top5_2").addEventListener("click", open_2);
 
 
     function open_1() {
+        console.log("open security");
         document.querySelector(".drop_top5_1").classList.remove("hidden")
         document.querySelector(".top5_1").addEventListener("click", close_1);
         document.querySelector(".top5_1").removeEventListener("click", open_1);
     }
 
+    function open_2() {
+        console.log("open cut costs");
+        document.querySelector(".drop_top5_2").classList.remove("hidden")
+        document.querySelector(".top5_2").addEventListener("click", close_2);
+        document.querySelector(".top5_2").removeEventListener("click", open_2);
+    }
+
     function close_1() {
+        console.log("close security");
         document.querySelector(".drop_top5_1").classList.add("hidden")
         document.querySelector(".top5_1").addEventListener("click", open_1);
         document.querySelector(".top5_1").removeEventListener("click", close_1);
+    }
 
-
-
+    function close_2() {
+        console.log("close cut costs");
+        document.querySelector(".drop_top5_2").classList.add("hidden")
+        document.querySelector(".top5_2").addEventListener("click", open_2);
+        document.querySelector(".top5_2").removeEventListener("click", close_2);
     }
 }
